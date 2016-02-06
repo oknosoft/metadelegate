a1feadc817bc5b465bfd6784f8c23c7165f55c98
unpete <info@oknosoft.ru>
2016-02-06 22:34:16 +0500

    v.0.0.1

--- src/view_events.js
+++ src/view_events.js
@@ -9,10 +9,70 @@
 $p.iface.view_events = function (cell) {
 
 	function OViewEvents(){
-		cell.attachHTMLString($p.injected_data['view_about.html']);
-		cell.cell.querySelector(".dhx_cell_cont_sidebar").style.overflow = "auto";
 
-		this.tb_nav = new $p.iface.OTooolBar({
+		var t = this;
+
+		function create_scheduler(){
+			//scheduler.config.xml_date="%Y-%m-%d %H:%i";
+			scheduler.config.first_hour = 8;
+			scheduler.config.last_hour = 22;
+
+			var sTabs = '<div class="dhx_cal_tab" name="day_tab" style="right:204px;"></div>'+
+				'<div class="dhx_cal_tab" name="week_tab" style="right:140px;"></div>'+
+				'<div class="dhx_cal_tab" name="month_tab" style="right:280px;"></div>'+
+				'<div class="dhx_cal_date"></div><div class="dhx_minical_icon">&nbsp;</div>';
+				//'<div class="dhx_cal_tab" name="timeline_tab" style="right:76px;"></div>';
+
+			t._scheduler = cell.attachScheduler(null, "week", sTabs);
+
+			t._scheduler.attachEvent("onBeforeViewChange", function(old_mode, old_date, mode, date){
+				//if(mode == "timeline"){
+				//	$p.msg.show_not_implemented();
+				//	return false;
+				//}
+				return true;
+			});
+		}
+
+		function hash_route(hprm) {
+
+			if(hprm.view == "events"){
+
+				if($p.eve.logged_in){
+
+					if(hprm.obj == "doc.ЗаказКлиента" && !$p.is_empty_guid(hprm.ref)){
+
+						if(hprm.frm != "doc")
+							setTimeout(function () {
+								$p.iface.set_hash(undefined, undefined, "doc");
+							});
+						else
+							show_doc(hprm.ref);
+
+
+					} else if($p.is_empty_guid(hprm.ref) || hprm.frm == "list"){
+
+						if(hprm.obj != "doc.ЗаказКлиента")
+							setTimeout(function () {
+								$p.iface.set_hash("doc.ЗаказКлиента");
+							});
+						else
+							show_list();
+					}
+				}
+
+				return false;
+			}
+
+		}
+
+		if(!window.dhtmlXScheduler){
+			$p.load_script("lib/dhtmlxscheduler/dhtmlxscheduler.min.js", "script", create_scheduler);
+			$p.load_script("lib/dhtmlxscheduler/dhtmlxscheduler.css", "link");
+		}else
+			create_scheduler();
+
+		t.tb_nav = new $p.iface.OTooolBar({
 			wrapper: cell.cell.querySelector(".dhx_cell_sidebar_hdr"),
 			class_name: 'md_otbnav',
 			width: '200px', height: '28px', top: '3px', right: '3px', name: 'right',
--- src/view_orders.js
+++ src/view_orders.js
@@ -10,21 +10,21 @@ $p.iface.view_orders = function (cell) {
 
 	function OViewOrders(){
 
-		var t = this;
+		var t = this, _tab,
+			_tabs = [
+				{id: "orders", text: '<i class="fa fa-suitcase"></i> Заказы', class_name: "doc.ЗаказКлиента", active: true},
+				{id: "pays", text: '<i class="fa fa-money"></i> Оплаты', class_name: "doc.ПриходныйКассовыйОрдер"},
+				{id: "shipments", text: '<i class="fa fa-shopping-bag"></i> Продажи', class_name: "doc.РеализацияТоваровУслуг"},
+				{id: "balance", text: '<i class="fa fa-balance-scale"></i> Баланс'}
+			];
 
 		function show_list(){
 
 			var _cell = t.carousel.cells("list"),
 				def_prm = {
 					hide_header: true,
-					date_from: new Date("2012-01-01")
-				},
-				tabs = [
-					{id: "orders", text: '<i class="fa fa-suitcase"></i> Заказы', active: true},
-					{id: "pays", text: '<i class="fa fa-money"></i> Оплаты'},
-					{id: "shipments", text: '<i class="fa fa-shopping-bag"></i> Продажи'},
-					{id: "balance", text: '<i class="fa fa-balance-scale"></i> Баланс'}
-				];
+					date_from: new Date((new Date()).getFullYear().toFixed() + "-01-01")
+				};
 
 			if(t.carousel.getActiveCell() != _cell)
 				_cell.setActive();
@@ -34,7 +34,16 @@ $p.iface.view_orders = function (cell) {
 				_cell.detachObject(true);
 				t.tabs = _cell.attachTabbar({
 					arrows_mode:    "auto",
-					tabs: tabs
+					tabs: _tabs
+				});
+
+				t.tabs.attachEvent("onSelect", function(id, lastId){
+					if(_tab && _tab.id != id){
+						var tab = _tabs.filter(function (val) { return val.id == id; })[0];
+						if(tab.class_name)
+							$p.iface.set_hash(tab.class_name, "");
+					}						
+					return true;
 				});
 
 				t._orders = $p.doc.ЗаказКлиента.form_list(t.tabs.cells("orders"), def_prm);
@@ -44,9 +53,11 @@ $p.iface.view_orders = function (cell) {
 
 				// TODO: заплата, исправляющая заголовки
 				setTimeout(function () {
-					tabs.forEach(function (tab) {
+					_tabs.forEach(function (tab) {
 						t.tabs.cells(tab.id).setText(tab.text);
-					})
+					});
+					if(_tab && t.tabs.getActiveTab() != _tab.id)
+						t.tabs.cells(_tab.id).setActive();
 				});
 
 			}
@@ -60,8 +71,8 @@ $p.iface.view_orders = function (cell) {
 			if(t.carousel.getActiveCell() != _cell)
 				_cell.setActive();
 
-			if(!_cell.ref || _cell.ref != ref)
-				$p.doc.ЗаказКлиента.form_obj(_cell, {
+			if(_tab && (!_cell.ref || _cell.ref != ref))
+				$p.md.mgr_by_class_name(_tab.class_name).form_obj(_cell, {
 						ref: ref,
 						bind_pwnd: true,
 						on_close: function () {
@@ -76,13 +87,32 @@ $p.iface.view_orders = function (cell) {
 		}
 
 
+		// переключает закладку в соответствии с hprm.obj при маршрутизации
+		function select_tab(obj){
+			_tab = undefined;
+			_tabs.forEach(function (tab) {
+				if(tab.class_name == obj){
+					if(t.tabs && t.tabs.getActiveTab() != tab.id)
+						t.tabs.cells(tab.id).setActive();
+					_tab = tab;
+				}
+			});
+		}
+
+		/**
+		 * Обработчик маршрутизации url
+		 * @param hprm
+		 * @return {boolean}
+		 */
 		function hash_route(hprm) {
 
 			if(hprm.view == "orders"){
 
 				if($p.eve.logged_in){
 
-					if(hprm.obj == "doc.ЗаказКлиента" && !$p.is_empty_guid(hprm.ref)){
+					select_tab(hprm.obj);
+
+					if(_tab && !$p.is_empty_guid(hprm.ref)){
 
 						if(hprm.frm != "doc")
 							setTimeout(function () {
@@ -94,7 +124,7 @@ $p.iface.view_orders = function (cell) {
 
 					} else if($p.is_empty_guid(hprm.ref) || hprm.frm == "list"){
 
-						if(hprm.obj != "doc.ЗаказКлиента")
+						if(!_tab)
 							setTimeout(function () {
 								$p.iface.set_hash("doc.ЗаказКлиента");
 							});
